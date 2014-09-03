f38cdeba86d5689308cbde64abc12814665a87e3
Cody Chan <int64ago@gmail.com>
2014-09-04 00:47:26 +0800

    chrome extension:apply desktop notification instead of alert

--- GetLink/js/background.js
+++ GetLink/js/background.js
@@ -8,6 +8,13 @@ chrome.contextMenus.create({
 	}
 });
 
+function showMsg(msgTitle, msgIcon, msgBody) {
+  new Notification(msgTitle, {
+    icon: msgIcon,
+    body: msgBody
+  });
+}
+
 function getlink(url){
 	var picUrl = 'http://getlink.sinaapp.com/ext?url='+url;
 	var xhr = new XMLHttpRequest();
@@ -15,11 +22,11 @@ function getlink(url){
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			var retUrl = xhr.responseText;
-			if(retUrl != 'none'){
+			if(retUrl != 'none' && retUrl.match("int64ago.qiniudn.com") != null){
 				copyTextToClipboard(retUrl);
-				alert('成功生成外链，已经复制到剪贴板');
+				showMsg("成功生成外链:-)", "images/icon48.png", "链接已经复制到剪贴板");
 			}else{
-				alert('无法生成外链，请下载后用getlink.sinaapp.com上传');
+				showMsg("生成外链失败！", "images/icon48_red.png", "请下载后在http://getlink.sinaapp.com/上传");
 			}
 		}
 	}
