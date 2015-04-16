838eaf9104949c9514630e45c0fd23e444f4e068
Cody Chan <int64ago@gmail.com>
2015-04-16 19:24:03 +0800

    Use Jquery instead of XHR

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-/*global Dropzone, console, alert*/
+/*global Dropzone, console, alert, $*/
 
 (function () {
     "use strict";
-    var xhr = new XMLHttpRequest(),
-        uptoken = '';
-    xhr.open('POST', 'http://qiniu.coding.io/uptoken', false);
-    xhr.onload = function (e) {
-        if (this.status === 200) {
-            uptoken = this.response;
-        } else {
-            alert('Oops! Get uptoken error.');
-        }
-    };
-    xhr.send();
+    var uptoken = '';
+    $.post('http://qiniu.coding.io/uptoken', function (data) {
+        uptoken = data;
+    }).fail(function () {
+        alert('Oops! Get uptoken error.');
+    });
     Dropzone.options.myAwesomeDropzone = {
         maxFilesize: 6,
         maxFiles: 50,
-        params: {
-            token: uptoken
-        },
         acceptedFiles: 'image/*',
         init: function () {
             var param = this.params;
@@ -26,6 +18,7 @@
                 var suffix = file.name.split('.').pop();
                 suffix = file.name.length - suffix.length <= 1 ? '' : suffix;
                 formData.append('key', Math.random().toString(36).substring(5) + '.' + suffix);
+                formData.append('token', uptoken);
             });
         }
     };
