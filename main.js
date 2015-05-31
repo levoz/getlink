e94e4d47086d31d8aa7d6a9534da0ba5efda3099
Cody Chan <int64ago@gmail.com>
2015-05-31 15:26:29 +0800

    Update

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
     Dropzone.options.myAwesomeDropzone = {
         maxFilesize: 6,
         maxFiles: 50,
-        acceptedFiles: 'image/*,.json',
+        acceptedFiles: 'image/*,.json,.txt',
         init: function () {
             var param = this.params;
             this.on('sending', function (file, xhr, formData) {
